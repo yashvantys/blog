3e2e194e427aeff5132aac37d370ff42e4d59ce8
Yashvant Yadav <yashvanty@gmail.com>
2023-09-25 09:38:58 +0530

    Added Auth Service using appwritter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import config from './config/config'
+
+function App() {
+  console.log(config.appwriteUrl)
+
+  return (
+    <>
+      <h1>A Blog with appWrite</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+    client = new Client()
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call other function
+                return this.login({ email, password })
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+
+}
+const authService = new AuthService()
+export default authService
